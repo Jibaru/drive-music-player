d81de06f8e4f08be8fb861b4071f2cc66299e575
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-19 01:05:29 -0500

    Created send mail service Service to send mail

---
+++ server/src/api/services/mail/send-mail.service.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+const sendMailService = async ({ to, subject, content }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST + "aasd",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: process.env.MAIL_USER, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text: content, // plain text body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    return info;
+  } catch (error) {
+    // console.error(error);
+    throw new Error(error.message);
+  }
+};
+
+module.exports = sendMailService;
