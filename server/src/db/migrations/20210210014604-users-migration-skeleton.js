f62c6ae3687f362245f5bbe4a3f0292b8275efbf
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-21 21:04:23 -0500

    Added email field to user migration Added field type string, unique and not null

--- server/src/db/migrations/20210210014604-users-migration-skeleton.js
+++ server/src/db/migrations/20210210014604-users-migration-skeleton.js
@@ -12,6 +12,11 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
+      email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
       password: {
         type: Sequelize.DataTypes.STRING,
         allowNull: false,
