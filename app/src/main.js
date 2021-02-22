bee661efc98b4a964a8334cb5e8c11711576ffa9
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-22 14:15:33 -0500

    Added routes and fortawesome component Added router and fontawesome to main app

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faList, faBars, faMusic } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import App from "./App.vue";
+import router from "./routes.js";
 
 const app = createApp(App);
+
+library.add(faList, faBars, faMusic);
+
+app.use(router);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
