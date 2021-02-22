746f7b61069d4df201cc59d4757e2767ae61b6ce
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-21 21:05:17 -0500

    Added field email to user model Added field of type string, unique and not null

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -7,6 +7,11 @@ module.exports = (sequelize, DataTypes) => {
         unique: true,
         allowNull: false,
       },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
