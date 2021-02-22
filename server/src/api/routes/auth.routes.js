bc662f97bb5840097c0830e34204a07b36d889b1
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-21 22:33:47 -0500

    Added pre-register route Added post route to pre-register user

--- server/src/api/routes/auth.routes.js
+++ server/src/api/routes/auth.routes.js
@@ -3,10 +3,16 @@ const app = require("express")();
 const ROUTES = {
   login: "/api/login",
   register: "/api/register",
+  preRegister: "/api/pre-register",
 };
 
 app.post(ROUTES.login, require("../controllers/auth/login.controller"));
 
 app.post(ROUTES.register, require("../controllers/auth/register.controller"));
+
+app.post(
+  ROUTES.preRegister,
+  require("../controllers/auth/pre-register.controller")
+);
 
 module.exports = app;
