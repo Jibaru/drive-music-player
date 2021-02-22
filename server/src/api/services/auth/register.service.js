d22148047009e361ccd0e1cd2af31c69bbd6cd44
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-21 22:32:24 -0500

    Added email to register service Added email on register sevice (from update user model)

--- server/src/api/services/auth/register.service.js
+++ server/src/api/services/auth/register.service.js
@@ -1,11 +1,12 @@
 const bcrypt = require("bcrypt");
 const { User } = require("../../../db/models/index");
 
-const registerService = async ({ username, password }) => {
+const registerService = async ({ username, email, password }) => {
   console.log("Slta rounds:", process.env.BCRYPT_SALT_ROUNDS);
   try {
     const userCreated = await User.create({
       username: username,
+      email: email,
       password: bcrypt.hashSync(
         password,
         parseInt(process.env.BCRYPT_SALT_ROUNDS)
