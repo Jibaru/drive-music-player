6fd3a39f98d3d6ce6af21d01cffa4bbc969bb0f8
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-21 22:33:07 -0500

    Added email to register controller Added email to body register controller

--- server/src/api/controllers/auth/register.controller.js
+++ server/src/api/controllers/auth/register.controller.js
@@ -1,12 +1,19 @@
+const getOneSignupInformationService = require("../../services/signup-information/get-one-signup-information.service");
 const registerService = require("../../services/auth/register.service");
 
 // POST: /api/register
-// body: username, password
+// body: username, password, email, pin
 const registerController = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, email, password, pin } = req.body;
 
   try {
-    const user = await registerService({ username, password });
+    const signupInformation = await getOneSignupInformationService(email);
+
+    if (signupInformation.pin !== pin) {
+      throw new Error("Verification code is invalid");
+    }
+
+    const user = await registerService({ username, email, password });
 
     return res.status(200).json({
       ok: true,
