189bb2477c24e5593bd20d2cc98f66dc45ce8b3a
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-22 22:51:55 -0500

    Added base icon button as global component Registered into main app file

--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./routes.js";
 
 import BaseButton from "./components/ui/BaseButton.vue";
+import BaseIconButton from "./components/ui/BaseIconButton.vue";
 
 import { FontAwesomeIcon } from "./plugins/fortawesome.js";
 
@@ -12,6 +13,7 @@ const app = createApp(App);
 app.use(router);
 
 app.component("base-button", BaseButton);
+app.component("base-icon-button", BaseIconButton);
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
