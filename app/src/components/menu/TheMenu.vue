2650016c2ebe167a42a86ef7c67e4d8343c52f48
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-22 21:05:18 -0500

    Created menu component Display user data and playlists

---
+++ app/src/components/menu/TheMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <user-data class="user-data" />
+    <the-songs-menu class="songs-menu" />
+  </div>
+</template>
+<script>
+import TheSongsMenu from "./TheSongsMenu.vue";
+import UserData from "../user/UserData.vue";
+
+export default {
+  components: {
+    TheSongsMenu,
+    UserData,
+  },
+};
+</script>
+<style scoped>
+div {
+  background: var(--app-primary-color);
+}
+
+.user-data {
+  height: 8rem;
+}
+
+.songs-menu {
+  height: calc(100% - 8rem);
+}
+</style>
