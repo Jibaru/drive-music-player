d1fa245649dcb3b68f7c345880201d4716633887
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-22 21:04:30 -0500

    Changed colors and shadows Changed colors and shadows

--- app/src/components/nav/TheNavbar.vue
+++ app/src/components/nav/TheNavbar.vue
@@ -50,7 +50,10 @@ export default {
 <style scoped>
 nav {
   width: 100%;
+  background-color: var(--app-primary-color);
   box-shadow: 0px -2px 2px 0px rgba(0, 0, 0, 0.3);
+  position: relative;
+  z-index: 10;
 }
 
 ul {
