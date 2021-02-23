c4487704f449a3040d0a5dcf7efcf99dbba81f4e
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-23 17:55:45 -0500

    Created mobile page Moved component mobile view into this component

---
+++ app/src/pages/mobile/MobilePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mobile-page-container">
+    <router-view class="main"></router-view>
+    <the-navbar class="navbar"></the-navbar>
+  </div>
+</template>
+<script>
+import TheNavbar from "../../components/nav/TheNavbar.vue";
+
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+<style scoped>
+.mobile-page-container {
+  height: 100vh;
+}
+
+.main {
+  height: calc(100vh - 3.5rem);
+}
+
+.navbar {
+  height: 3.5rem;
+}
+</style>
