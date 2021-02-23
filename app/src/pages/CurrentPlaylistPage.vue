7e10337c724d0e220f952c51287f3729724e11c0
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-23 17:17:13 -0500

    Added current song list component Added current song list component

--- app/src/pages/CurrentPlaylistPage.vue
+++ app/src/pages/CurrentPlaylistPage.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>current playlist page</h1>
+  <div>
+    <current-song-list class="current-song-list" />
+  </div>
 </template>
+<script>
+import CurrentSongList from "../components/song/CurrentSongList.vue";
+
+export default {
+  components: {
+    CurrentSongList,
+  },
+};
+</script>
+<style scoped>
+div {
+  height: 100%;
+  background-color: var(--app-primary-color);
+}
+
+.current-song-list {
+  height: 100%;
+}
+</style>
