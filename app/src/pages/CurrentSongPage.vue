459cbc2456e0ac14c12957a02ca9479202bc9db5
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-23 14:13:59 -0500

    Added current song component to current song page Added the current song component

--- app/src/pages/CurrentSongPage.vue
+++ app/src/pages/CurrentSongPage.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>current song page</h1>
+  <div>
+    <current-song class="current-song" />
+  </div>
 </template>
+<script>
+import CurrentSong from "../components/song/CurrentSong.vue";
+
+export default {
+  components: {
+    CurrentSong,
+  },
+};
+</script>
+<style scoped>
+div {
+  height: 100%;
+}
+
+.current-song {
+  height: 100%;
+}
+</style>
