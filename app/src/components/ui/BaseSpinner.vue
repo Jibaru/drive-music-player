d18c8574fa867877d3bc864e84fc2a9c5cde44e6
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-24 13:17:14 -0500

    Created base spinner Created base loader spinner

---
+++ app/src/components/ui/BaseSpinner.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container" :class="{ center: centered }">
+    <div class="loader" :style="styles"></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      required: false,
+      default: 3,
+    },
+    centered: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    styles() {
+      return {
+        width: `${this.size}rem`,
+        height: `${this.size}rem`,
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+.container {
+  width: fit-content;
+}
+
+.loader {
+  border: 0.5rem solid #f3f3f3;
+  border-top: 0.5rem solid var(--app-secondary-color);
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.center {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
