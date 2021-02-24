95bb878c326f2f6e22b887443ad8caba78d94c43
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-24 17:20:22 -0500

    Added vuex logic to register page Added vuex logic

--- app/src/pages/RegisterPage.vue
+++ app/src/pages/RegisterPage.vue
@@ -2,32 +2,68 @@
   <main>
     <div class="card">
       <h1>Register a new Account</h1>
-      <form action="">
-        <section>
+      <form @submit.prevent="submitForm">
+        <section v-show="!isPreRegistered">
           <div class="form-control">
             <label for="username">Username</label>
-            <input type="text" id="username" />
+            <input
+              type="text"
+              id="username"
+              v-model.trim="username.val"
+              @keyup="isPreRegisterFormValid()"
+            />
+            <span v-show="!username.isValid" class="error">
+              Username should not be blank
+            </span>
           </div>
           <div class="form-control">
             <label for="email">Email</label>
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              v-model.trim="email.val"
+              @keyup="isPreRegisterFormValid()"
+            />
+            <span v-show="!email.isValid" class="error">
+              Email should not be blank
+            </span>
           </div>
           <div class="form-control">
             <label for="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              v-model.trim="password.val"
+              @keyup="isPreRegisterFormValid()"
+              autocomplete
+            />
+            <span v-show="!password.isValid" class="error">
+              Password should not be blank and must be at least 6 characters
+            </span>
           </div>
-          <base-button d-block>
+          <base-button v-if="!isLoading" d-block>
             <b>Sign In</b>
           </base-button>
+          <base-spinner v-else centered />
         </section>
-        <section>
+        <section v-show="isPreRegistered">
           <div class="form-control">
             <label for="code">Verification Code</label>
-            <input type="text" id="code" class="verification-input" />
+            <input
+              type="text"
+              id="code"
+              class="verification-input"
+              v-model.trim="pin.val"
+              @keyup="isRegisterFormValid()"
+            />
+            <span v-show="!pin.isValid" class="error">
+              Pin should not be blank
+            </span>
           </div>
-          <base-button d-block>
+          <base-button d-block v-if="!isLoading">
             <b>Check and Finish</b>
           </base-button>
+          <base-spinner v-else centered />
         </section>
         <div class="card-footer">
           <router-link to="/login">Let's Start!</router-link>
@@ -43,6 +79,126 @@
     </footer>
   </main>
 </template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      username: {
+        val: "",
+        isValid: true,
+      },
+      email: {
+        val: "",
+        isValid: true,
+      },
+      password: {
+        val: "",
+        isValid: true,
+      },
+      pin: {
+        val: "",
+        isValid: true,
+      },
+      isLoading: false,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      isPreRegistered: "auth/isPreRegistered",
+      registered: "auth/registered",
+    }),
+  },
+  watch: {
+    registered(newVal) {
+      if (newVal) {
+        this.clearForm();
+      }
+    },
+  },
+  methods: {
+    ...mapActions({
+      preRegisterAction: "auth/preRegister",
+      registerAction: "auth/register",
+      newRegister: "auth/newRegister",
+    }),
+    isPreRegisterFormValid() {
+      if (this.username.val === "") {
+        this.username.isValid = false;
+      } else {
+        this.username.isValid = true;
+      }
+
+      if (this.email.val === "") {
+        this.email.isValid = false;
+      } else {
+        this.email.isValid = true;
+      }
+
+      if (this.password.val === "" || this.password.val.length < 6) {
+        this.password.isValid = false;
+      } else {
+        this.password.isValid = true;
+      }
+
+      return (
+        this.username.isValid && this.email.isValid && this.password.isValid
+      );
+    },
+    isRegisterFormValid() {
+      if (this.pin.val === "") {
+        this.pin.isValid = false;
+      } else {
+        this.pin.isValid = true;
+      }
+
+      return this.pin.isValid;
+    },
+    async submitForm() {
+      this.isLoading = true;
+
+      if (!this.isPreRegistered) {
+        if (!this.isPreRegisterFormValid()) {
+          this.isLoading = false;
+          return;
+        }
+
+        this.newRegister();
+        await this.preRegisterAction({
+          username: this.username.val,
+          email: this.email.val,
+          password: this.password.val,
+        });
+      } else {
+        if (!this.isRegisterFormValid()) {
+          this.isLoading = false;
+          return;
+        }
+
+        await this.registerAction({
+          username: this.username.val,
+          email: this.email.val,
+          password: this.password.val,
+          pin: this.pin.val,
+        });
+      }
+
+      this.isLoading = false;
+    },
+    clearForm() {
+      this.username.val = "";
+      this.email.val = "";
+      this.password.val = "";
+    },
+  },
+  beforeRouteEnter(_, _2, next) {
+    next((vm) => {
+      vm.newRegister();
+    });
+  },
+};
+</script>
 <style scoped>
 main {
   width: 100%;
@@ -95,6 +251,12 @@ main {
 .form-control input:active {
   outline: none;
   box-shadow: 0 0 2px 1px var(--app-secondary-color);
+}
+
+.form-control .error {
+  font-size: 0.8rem;
+  text-align: end;
+  color: var(--app-danger-color);
 }
 
 .verification-input {
