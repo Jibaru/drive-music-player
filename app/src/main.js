908f26dba1f4df9b0fc7046adddeccbd6f27edbc
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-24 12:50:07 -0500

    Added store Added store to main app

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./routes.js";
+import store from "./store/index.js";
 
 import BaseButton from "./components/ui/BaseButton.vue";
 import BaseIconButton from "./components/ui/BaseIconButton.vue";
@@ -12,6 +13,7 @@ import { FontAwesomeIcon } from "./plugins/fortawesome.js";
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
 app.component("base-button", BaseButton);
 app.component("base-icon-button", BaseIconButton);
