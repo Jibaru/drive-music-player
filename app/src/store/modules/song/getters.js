77b45eb3bbf2d5352b6e1bdebd774aa4c09e5c69
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-25 12:38:42 -0500

    Added favorite songs getters

--- app/src/store/modules/song/getters.js
+++ app/src/store/modules/song/getters.js
@@ -2,8 +2,14 @@ export default {
   allSongs(state) {
     return state.songs;
   },
+  favoriteSongs(state) {
+    return state.songs.filter((song) => song.isFavorite);
+  },
   songsEmpty(state) {
     return state.songs.length === 0;
+  },
+  favoriteSongsEmpty(_, getters) {
+    return getters.favoriteSongs.length === 0;
   },
   fetchingSongs(state) {
     return state.fetchingSongs;
