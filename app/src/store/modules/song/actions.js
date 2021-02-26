ac6482808a3cd872e43946e87c31d30fc830a707
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-25 19:41:02 -0500

    Added service into toggle song favorite action

--- app/src/store/modules/song/actions.js
+++ app/src/store/modules/song/actions.js
@@ -1,4 +1,5 @@
 import getUserSongsService from "../../../services/song/get-user-songs.service.js";
+import updateFavoriteUserSongService from "../../../services/song/update-favorite-user-song.service.js";
 
 export default {
   async fetchSongs(context) {
@@ -19,7 +20,29 @@ export default {
       context.commit("setIsFetching", { val: false });
     }
   },
-  toggleSongFavorite(context, { songId, val }) {
-    context.commit("toggleSongFavorite", { songId, val });
+  async toggleSongFavorite(context, { songId, val }) {
+    try {
+      const userId = context.rootGetters["auth/userId"];
+      const token = context.rootGetters["auth/token"];
+      const message = await updateFavoriteUserSongService({
+        userId,
+        songId,
+        isFavorite: val,
+        token,
+      });
+
+      context.commit("toggleSongFavorite", { songId, val });
+      context.dispatch(
+        "showGlobalSnackBarMessage",
+        { message },
+        { root: true }
+      );
+    } catch (error) {
+      context.dispatch(
+        "showGlobalSnackBarMessage",
+        { message: error.message },
+        { root: true }
+      );
+    }
   },
 };
