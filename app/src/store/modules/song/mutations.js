15494145a1f016326d7564c1eb59345529f081b7
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-25 19:38:27 -0500

    Fixed toggle song favorite mutation

--- app/src/store/modules/song/mutations.js
+++ app/src/store/modules/song/mutations.js
@@ -7,19 +7,6 @@ export default {
   },
   toggleSongFavorite(state, { songId, val }) {
     const index = state.songs.findIndex((song) => song.id === songId);
-    if (index !== -1) {
-      const song = { ...state.songs[index] };
-      song.isFavorite = val;
-      // Vue.set(state.songs, index, song);
-      // state.songs[index].isFavorite = val;
-      // const songs = [...state.songs];
-      // const song = { ...songs[index] };
-
-      // state.songs = [
-      //   ...state.songs.splice(0, index),
-      //   song,
-      //   ...songs.splice(index, songs.length - 1),
-      // ];
-    }
+    state.songs[index].isFavorite = val;
   },
 };
