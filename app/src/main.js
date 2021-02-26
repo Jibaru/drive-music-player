a0cc7ff9e70d9c87b2973e00eb20446288082067
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-25 22:46:32 -0500

    Added base dialog as global component

--- app/src/main.js
+++ app/src/main.js
@@ -8,6 +8,7 @@ import BaseButton from "./components/ui/BaseButton.vue";
 import BaseIconButton from "./components/ui/BaseIconButton.vue";
 import BaseSnackbar from "./components/ui/BaseSnackbar.vue";
 import BaseSpinner from "./components/ui/BaseSpinner.vue";
+import BaseDialog from "./components/ui/BaseDialog.vue";
 
 import { FontAwesomeIcon } from "./plugins/fortawesome.js";
 
@@ -21,5 +22,6 @@ app.component("base-icon-button", BaseIconButton);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.component("base-snackbar", BaseSnackbar);
 app.component("base-spinner", BaseSpinner);
+app.component("base-dialog", BaseDialog);
 
 app.mount("#app");
