58b23a6e8da434dd306653c0f535f11e96b80587
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-26 13:51:52 -0500

    Created base confirm dialog component

---
+++ app/src/components/ui/BaseConfirmDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <base-dialog
+    :title="title"
+    :open="open"
+    @close="$emit('cancel')"
+    :type="type"
+    footered
+  >
+    <slot></slot>
+    <template #actions>
+      <div v-if="!isLoading">
+        <base-button @click="$emit('confirm')">
+          Yes
+        </base-button>
+        <base-button @click="$emit('cancel')" color="danger">
+          Cancel
+        </base-button>
+      </div>
+      <base-spinner v-else centered />
+    </template>
+  </base-dialog>
+</template>
+<script>
+export default {
+  emits: ["cancel", "confirm"],
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    open: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "small",
+    },
+    isLoading: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
