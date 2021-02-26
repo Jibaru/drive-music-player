a012f24774e8fd80f5f874fb3885c7dca716fd7d
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-26 11:18:58 -0500

    Created create user playlist service

---
+++ app/src/services/playlist/create-user-playlist.service.js
@@ -0,0 +1,24 @@
+import { createFormUrlEncoded } from "../../utils/fetch.utils.js";
+import { CREATE_USER_PLAYLIST } from "../../config/api-uri.js";
+
+const getUserPlaylistsService = async (name, userId, token) => {
+  const uri = CREATE_USER_PLAYLIST.replace(":userId", `${userId}`);
+  const response = await fetch(process.env.VUE_APP_API_BASE + uri, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      token: token,
+    },
+    body: createFormUrlEncoded({
+      name,
+    }),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return data.message;
+  } else {
+    throw new Error(data.error);
+  }
+};
+
+export default getUserPlaylistsService;
