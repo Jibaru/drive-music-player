039db150809eea22e18bfa6089336d6d274575a9
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-27 18:16:03 -0500

    Added a lot of actions Added an action for each mutation

--- app/src/store/modules/current-playback/actions.js
+++ app/src/store/modules/current-playback/actions.js
@@ -1,1 +1,25 @@
-export default {};
+export default {
+  setSongAndPlay(context, { song }) {
+    context.commit("setCurrentSong", {
+      song,
+      onLoadedSong: () => {
+        context.commit("changeCurrentVolume", {
+          percentage: context.getters.currentVolume,
+        });
+        context.commit("play");
+      },
+    });
+  },
+  playSong(context) {
+    context.commit("play");
+  },
+  pauseSong(context) {
+    context.commit("pause");
+  },
+  changeCurrentTimestamp(context, { timestamp }) {
+    context.commit("changeCurrentTimestamp", { timestamp });
+  },
+  changeCurrentVolume(context, { percentage }) {
+    context.commit("changeCurrentVolume", { percentage });
+  },
+};
