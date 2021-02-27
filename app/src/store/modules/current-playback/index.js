0424b06c98e95bf1a5e7d6ae515c95ed26ceed6e
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-27 18:09:46 -0500

    Added more song playback info Added current timestamp and time, volume, play status, and more The current song is an object with all attributes of a song model (api) The current timestamp contains the exact playback moment The current total time is a value copy of the song duration The current volumen is a value between 0 and 100 The is playing attribute is a boolean can indicates if the song is playing

--- app/src/store/modules/current-playback/index.js
+++ app/src/store/modules/current-playback/index.js
@@ -7,8 +7,20 @@ export default {
   state() {
     return {
       songsPlaylist: [],
-      currentSong: null,
+      currentSong: {
+        id: null,
+        name: null,
+        songUrl: null,
+        imageUrl: null,
+        timesPlayed: 0,
+        totalDuration: null,
+      },
       playInstance: null,
+      currentTimestamp: 0,
+      currentTotalTime: 0,
+      timeStampInterval: null,
+      currentVolume: 100,
+      isPlaying: false,
     };
   },
   mutations,
