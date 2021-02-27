9620a0278921957a9e89f5da18787a31ba26a175
Jibaru <ignacioruedaboada@hotmail.com>
2021-02-27 11:27:45 -0500

    Added add song to playlists dialog component

--- app/src/components/song/FavoriteSongsList.vue
+++ app/src/components/song/FavoriteSongsList.vue
@@ -1,29 +1,49 @@
 <template>
-  <ul class="song-list" v-if="!isLoading && !favoriteSongsEmpty">
-    <song-item
-      v-for="song in favoriteSongs"
-      :key="song.id"
-      :song-id="song.id"
-      :song-name="song.name"
-      :image-url="song.imageUrl"
-      :times-played="song.timesPlayed"
-      :is-favorite="song.isFavorite"
-      :duration="song.duration"
-      @click-favorite-icon="toggleFavorite($event, song.id)"
+  <div class="container">
+    <ul class="song-list" v-if="!isLoading && !favoriteSongsEmpty">
+      <song-item
+        v-for="song in favoriteSongs"
+        :key="song.id"
+        :song-id="song.id"
+        :song-name="song.name"
+        :image-url="song.imageUrl"
+        :times-played="song.timesPlayed"
+        :is-favorite="song.isFavorite"
+        :duration="song.duration"
+        @click-favorite-icon="toggleFavorite($event, song.id)"
+        @add-to-playlists="openAddSongToPlaylistsDialog(song.id)"
+      />
+    </ul>
+    <base-spinner v-else-if="isLoading" centered full-height />
+    <div v-else-if="favoriteSongsEmpty" class="empty-message">
+      Empty
+    </div>
+    <add-song-to-playlists-dialog
+      :open="isAddSongToPlaylistsDialogOpen"
+      @close="closeAddSongToPlaylistsDialog"
+      :songName="selectedSong.name"
+      :songId="selectedSong.id"
     />
-  </ul>
-  <base-spinner v-else-if="isLoading" centered full-height />
-  <div v-else-if="favoriteSongsEmpty" class="empty-message">
-    Empty
   </div>
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
 import SongItem from "./SongItem.vue";
+import AddSongToPlaylistsDialog from "./AddSongToPlaylistsDialog.vue";
 
 export default {
   components: {
     SongItem,
+    AddSongToPlaylistsDialog,
+  },
+  data() {
+    return {
+      isAddSongToPlaylistsDialogOpen: false,
+      selectedSong: {
+        id: null,
+        name: null,
+      },
+    };
   },
   computed: {
     ...mapGetters({
@@ -36,13 +56,29 @@ export default {
     ...mapActions({
       toggleSongFavorite: "song/toggleSongFavorite",
     }),
+    findAndSetSong(id) {
+      const song = this.favoriteSongs.find((s) => s.id == id);
+      this.selectedSong.name = song.name;
+      this.selectedSong.id = song.id;
+    },
     toggleFavorite(val, songId) {
       this.toggleSongFavorite({ songId, val });
+    },
+    openAddSongToPlaylistsDialog(songId) {
+      this.findAndSetSong(songId);
+      this.isAddSongToPlaylistsDialogOpen = true;
+    },
+    closeAddSongToPlaylistsDialog() {
+      this.isAddSongToPlaylistsDialogOpen = false;
     },
   },
 };
 </script>
 <style scoped>
+.container {
+  height: 100%;
+}
+
 .song-list {
   display: block;
   overflow-y: scroll;
