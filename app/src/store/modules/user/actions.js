4b833415137fcdc03526c67a097497044be4037a
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-26 21:33:52 -0500

    Added refresh drive songs action

--- app/src/store/modules/user/actions.js
+++ app/src/store/modules/user/actions.js
@@ -1,4 +1,5 @@
 import updateUserRootDriveKeyService from "../../../services/user/update-user-root-drive-key.service.js";
+import refreshUserSongsService from "../../../services/user/refresh-user-songs.service.js";
 
 export default {
   async updateRootDriveKey(context, { rootDriveKey }) {
@@ -28,6 +29,31 @@ export default {
         { message: error.message },
         { root: true }
       );
+    }
+  },
+  async refreshDriveSongs(context) {
+    context.commit("setIsRefreshingDriveSongs", { val: true });
+    try {
+      const userId = context.rootGetters["auth/userId"];
+      const token = context.rootGetters["auth/token"];
+
+      const message = await refreshUserSongsService(userId, token);
+
+      context.dispatch("song/fetchSongs", null, { root: true });
+      context.dispatch("playlist/fetchPlaylists", null, { root: true });
+      context.dispatch(
+        "showGlobalSnackBarMessage",
+        { message },
+        { root: true }
+      );
+    } catch (error) {
+      context.dispatch(
+        "showGlobalSnackBarMessage",
+        { message: error.message },
+        { root: true }
+      );
+    } finally {
+      context.commit("setIsRefreshingDriveSongs", { val: false });
     }
   },
 };
