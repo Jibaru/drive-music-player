900cd90587b7c925141be515349d0b9d134b204e
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-26 19:06:25 -0500

    Added mutations to add or remove song id to playlist

--- app/src/store/modules/playlist/mutations.js
+++ app/src/store/modules/playlist/mutations.js
@@ -2,6 +2,23 @@ export default {
   setPlaylists(state, { playlists }) {
     state.playlists = playlists;
   },
+  addSongIdToPlaylist(state, { playlistId, songId }) {
+    const index = state.playlists.findIndex(
+      (playlist) => playlist.id === playlistId
+    );
+    state.playlists[index].songs.push({
+      id: songId,
+    });
+  },
+  removeSongIdFromPlaylist(state, { playlistId, songId }) {
+    const playlistIndex = state.playlists.findIndex(
+      (playlist) => playlist.id === playlistId
+    );
+    const songIndex = state.playlists[playlistIndex].songs.findIndex(
+      (song) => song.id === songId
+    );
+    state.playlists[playlistIndex].songs.splice(songIndex, 1);
+  },
   setIsFetching(state, { val }) {
     state.fetchingPlaylists = val;
   },
@@ -10,5 +27,11 @@ export default {
   },
   setIsDeletingPlaylist(state, { val }) {
     state.deletingPlaylist = val;
+  },
+  setIsAddingSongToPlaylist(state, { val }) {
+    state.addingSongToPlaylist = val;
+  },
+  setIsRemovingSongFromPlaylist(state, { val }) {
+    state.removingSongFromPlaylist = val;
   },
 };
