bedc8aa8c70fc54e62d3648cd8040e81578e8e6a
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-27 18:17:25 -0500

    Added events and props

--- app/src/components/play-control/PlayControl.vue
+++ app/src/components/play-control/PlayControl.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <elapsed-time-bar :elapsed-time="111" :total-time="161" />
+    <elapsed-time-bar
+      :elapsed-time="elapsedTime"
+      :total-time="totalTime"
+      @changed-bar="$emit('changed-bar', $event)"
+      @finished="$emit('play-finished')"
+    />
     <div class="controls">
       <div class="reproduction">
         <base-icon-button icon="backward" unbackground />
-        <base-icon-button icon="play" unbackground big />
+        <base-icon-button
+          :icon="statusIcon"
+          unbackground
+          big
+          @click="changePlaybackStatus"
+          :key="refreshIcon"
+        />
         <base-icon-button icon="forward" unbackground />
       </div>
-      <volume-control :volume="45" />
+      <volume-control
+        :volume="currentVolume"
+        @changed-volume="$emit('changed-volume', $event)"
+      />
     </div>
   </div>
 </template>
@@ -19,6 +33,49 @@ export default {
   components: {
     ElapsedTimeBar,
     VolumeControl,
+  },
+  emits: ["changed-bar", "changed-volume", "pause", "play", "play-finished"],
+  props: {
+    elapsedTime: {
+      type: Number,
+      required: true,
+    },
+    totalTime: {
+      type: Number,
+      required: true,
+    },
+    currentVolume: {
+      type: Number,
+      required: true,
+    },
+    isPlaying: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      refreshIcon: 0,
+    };
+  },
+  computed: {
+    statusIcon() {
+      return this.isPlaying ? "pause" : "play";
+    },
+  },
+  watch: {
+    isPlaying() {
+      this.refreshIcon++;
+    },
+  },
+  methods: {
+    changePlaybackStatus() {
+      if (this.isPlaying) {
+        this.$emit("pause");
+      } else {
+        this.$emit("play");
+      }
+    },
   },
 };
 </script>
