e9d60915b77cc70d587886d6445ea166c94b14b7
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-27 18:14:54 -0500

    Added a lot of getters Added current song, current timestamp, current total time, volume and is playing

--- app/src/store/modules/current-playback/getters.js
+++ app/src/store/modules/current-playback/getters.js
@@ -1,1 +1,17 @@
-export default {};
+export default {
+  currentSong(state) {
+    return state.currentSong;
+  },
+  currentTimestamp(state) {
+    return state.currentTimestamp;
+  },
+  currentTotalTime(state) {
+    return state.currentTotalTime;
+  },
+  currentVolume(state) {
+    return state.currentVolume;
+  },
+  isPlaying(state) {
+    return state.isPlaying;
+  },
+};
