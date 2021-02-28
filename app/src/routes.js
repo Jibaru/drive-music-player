6f0aacb2356a3c6f7f173cd17bbff5584d4b4fb3
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-28 13:57:41 -0500

    Added route hook to redirect if user is authenticated

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "./store/index.js";
 
 import LoginPage from "./pages/LoginPage.vue";
 import RegisterPage from "./pages/RegisterPage.vue";
@@ -20,22 +21,36 @@ const routes = [
     name: "Login",
     path: "/login",
     component: LoginPage,
+    meta: {
+      requiresAuth: false,
+      requiresUnauth: true,
+    },
   },
   {
     name: "Register",
     path: "/register",
     component: RegisterPage,
+    meta: {
+      requiresAuth: false,
+      requiresUnauth: true,
+    },
   },
   {
     name: "Home",
     path: "/home",
     component: HomePage,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     name: "MobilePage",
     path: "/mobile",
     component: MobilePage,
     redirect: "/mobile/menu",
+    meta: {
+      requiresAuth: true,
+    },
     children: [
       {
         name: "Menu",
@@ -54,11 +69,25 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/:notFound(.*)",
+    redirect: "/",
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(function(to, _, next) {
+  if (!to.meta.requiresAuth && !store.getters["auth/isAuthenticated"]) {
+    next();
+  } else if (to.meta.requiresAuth && !store.getters["auth/isAuthenticated"]) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
