870fed3aa016273698e4eca32d87e0bd585e17c5
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-28 16:45:57 -0500

    Changed controller name Replaced seconds to milliseconds

--- server/src/api/controllers/user/set-duration-milliseconds-user-song.controller.js
+++ server/src/api/controllers/user/set-duration-milliseconds-user-song.controller.js
@@ -3,14 +3,14 @@ const containsUserSongService = require("../../services/songs/contains-user-song
 
 // POST: /api/users/:userId/songs/:songId/set-duration-seconds
 // body: durationSeconds
-const setDurationSecondsUserSongController = async (req, res) => {
+const setDurationMillisecondsUserSongController = async (req, res) => {
   const { userId, songId } = req.params;
-  const { durationSeconds } = req.body;
+  const { durationMilliseconds } = req.body;
 
   try {
     await containsUserSongService({ songId, userId });
 
-    const message = await updateSongService(songId, { durationSeconds });
+    const message = await updateSongService(songId, { durationMilliseconds });
 
     return res.status(200).json({
       ok: true,
@@ -24,4 +24,4 @@ const setDurationSecondsUserSongController = async (req, res) => {
   }
 };
 
-module.exports = setDurationSecondsUserSongController;
+module.exports = setDurationMillisecondsUserSongController;
