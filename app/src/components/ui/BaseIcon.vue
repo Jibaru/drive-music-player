d10c13c82d43d0597f5ac739dd64a7e3c8dee075
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-27 21:50:35 -0500

    Created base icon component to wrap font awesome icon close #1

---
+++ app/src/components/ui/BaseIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <font-awesome-icon :icon="icon" :key="updateCounter" />
+</template>
+<script>
+export default {
+  props: {
+    icon: {
+      type: [Array, String],
+      required: true,
+    },
+  },
+  data() {
+    return {
+      updateCounter: 0,
+    };
+  },
+  watch: {
+    icon() {
+      this.updateCounter++;
+    },
+  },
+};
+</script>
--- app/src/main.js
+++ app/src/main.js
@@ -10,6 +10,7 @@ import BaseSnackbar from "./components/ui/BaseSnackbar.vue";
 import BaseSpinner from "./components/ui/BaseSpinner.vue";
 import BaseDialog from "./components/ui/BaseDialog.vue";
 import BaseConfirmDialog from "./components/ui/BaseConfirmDialog.vue";
+import BaseIcon from "./components/ui/BaseIcon.vue";
 
 import { FontAwesomeIcon } from "./plugins/fortawesome.js";
 
@@ -25,5 +26,6 @@ app.component("base-snackbar", BaseSnackbar);
 app.component("base-spinner", BaseSpinner);
 app.component("base-dialog", BaseDialog);
 app.component("base-confirm-dialog", BaseConfirmDialog);
+app.component("base-icon", BaseIcon);
 
 app.mount("#app");
