53200f39609f7a87dbe3a46f330e65467e305058
Ignacio <ignacioruedaboada@hotmail.com>
2021-02-28 20:58:25 -0500

    Added db instance name env variable

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -17,7 +17,19 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
+    // host: process.env.DB_HOSTNAME,
     dialect: "mysql",
+    host: "/cloudsql/" + process.env.DB_INSTANCE_NAME,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
+      // same as host string above
+      socketPath: "/cloudsql/" + process.env.DB_INSTANCE_NAME,
+    },
   },
 };
